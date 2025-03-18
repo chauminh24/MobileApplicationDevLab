5c76abb3b78fb0c4b9a477cef1773982ca7d5b72
chauminh24 <114486211+chauminh24@users.noreply.github.com>
2025-03-18 14:46:51 +0000

    Add files via upload

---
+++ Lab04_05/dynamic_text_card/cache/19.2.0/dynamic-text-card/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,43 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-MDT75G4I.js";
+import "./chunk-6CMH6M6F.js";
+import "./chunk-6SU6ZFWW.js";
+import "./chunk-CLJS5SDT.js";
+import "./chunk-2E6NICEI.js";
+import "./chunk-VV6JI5M5.js";
+import "./chunk-6R2DQ5NE.js";
+import "./chunk-ZUJ64LXG.js";
+import "./chunk-XCIYP5SE.js";
+import "./chunk-OYTRG5F6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
